92157ac41d7de128bd6fb2160ed0911e1a713848
Albertolema <alberto.lema.blanco@gmail.com>
2022-02-25 11:56:37 +0100

    Update prueba_sftp.py

--- sftp/prueba_sftp.py
+++ sftp/prueba_sftp.py
@@ -4,9 +4,9 @@ from getpass import getpass
 # Tengo que maniobrar en ssh-add...
 
 CLAVE = getpass('Clave: ')
-HOST = 'asterix.isciii.es'
+HOST = ''
 PUERTO = 22
-USUARIO = 'alberto.lema'
+USUARIO = ''
 
 # Inicia un cliente SSH
 ssh_client = paramiko.SSHClient()
@@ -15,6 +15,6 @@ ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 ssh_client.connect(HOST, 22, USUARIO, CLAVE, allow_agent=False,look_for_keys=False)
 
-entrada, salida, error = ssh_client.exec_command('cd /data/bi/research/20211020_VIRALRECON-TEST_AL-EK-SM-SV_P/ANALYSIS | ls -la')
+entrada, salida, error = ssh_client.exec_command('cd path | ls -la')
 print (salida.read())
 ssh_client.close()
