f1d49721d2b10dd639ec84c984d466d5b0683afb
Alema91 <alberto.lema.blanco@gmail.com>
2022-03-23 11:46:43 +0100

    Libraries plot 2

--- qc_results/05_analysis_qc.R
+++ qc_results/05_analysis_qc.R
@@ -57,14 +57,34 @@ ggsave("Graficos/qc_boxplot_ejecucion.png")
 qc_categorias <- read_excel(dir_excel[1], sheet = 1)
 
 categorias_data <- data.frame(
-  id = as.character(qc_categorias$ID),
-  platform_1 = as.character(qc_categorias$sequencing_platforms_1),
-  platform_2 = as.character(qc_categorias$sequencing_platforms_2),
-  librerias = as.character(qc_categorias$libraries_2)
+    id = as.character(qc_categorias$ID),
+    platform_1 = factor(qc_categorias$sequencing_platforms_1, levels = c("Illumina", "Nanopore", "Ion Torrent")),
+    platform_2 = as.character(qc_categorias$sequencing_platforms_2),
+    librerias = as.character(qc_categorias$libraries_2)
 )
 
-#### boxplot ejecucion ----
+#### levels librerias ----
 
-ggplot(fechas_data, aes(y = ejecucion)) +
-    geom_boxplot() +
-    labs(y = "Execution time (days)", x = "")
+categorias_data$librerias <- factor(categorias_data$librerias, levels = c(c(
+    "ABL DeepChek NGS kit",
+    "Illumina DNA Prep Tagmentation",
+    "Illumina COVIDSeq Test kit",
+    "NEBNext® Ultra™ II FS DNA Library Prep Kit for Illumina",
+    "NEBNext® ARTIC SARS-CoV-2 FS kit for Illumina",
+    "Ion Xpress kit",
+    "Ion AmpliSeq Kit for Chef DL8 kit",
+    "NEBNext® Fast DNA Library Prep Set for Ion Torrent™ kit",
+    "ViroKey SQ FLEX SARS-CoV-2 Genotyping Assay (RUO) for Ion Torrent",
+    "Ion Code kit",
+    "Ion AmpliSeq™ SARS-CoV-2 Insight Research Assay - GS Manual",
+    "Oxford Nanopore Sequencing Kit"
+)))
+
+#### plot librerias ----
+
+ggplot(subset(categorias_data, !is.na(librerias)), aes(librerias, fill = platform_1)) +
+    geom_bar() +
+    guides(fill = guide_legend(title = "Platform")) +
+    labs(y = "", x = "") +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+ggsave("Graficos/qc_barplot_librerias.png")
