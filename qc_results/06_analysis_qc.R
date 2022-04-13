4e63934cfa50562dfbf9c3fdb05cdcb0c5450708
Alema91 <alberto.lema.blanco@gmail.com>
2022-04-13 12:54:28 +0200

    Final plots for qc doc: final

--- qc_results/06_analysis_qc.R
+++ qc_results/06_analysis_qc.R
@@ -161,7 +161,7 @@ virushost_data$tipo <- revalue(virushost_data$tipo, c("host" = "Huésped", "viru
 ggplot(virushost_data, aes(x = muestra2, y = porcentaje)) +
     geom_boxplot(fill = "#1F77B4") +
     facet_grid(plataforma ~ tipo) +
-    labs(x = "", y = "", title = "") +
+    labs(x = "", y = "%", title = "") +
     theme(
         text = element_text(size = 22),
         axis.text.x = element_text(vjust = 1, hjust = 1, angle = 45)
@@ -479,7 +479,7 @@ p1<- ggplot(subset(variants_data, plataforma == "Illumina"), aes(x = software, y
         sec.axis = sec_axis(trans=~./24, name="")
     ) +
     guides(color = guide_legend(title = "Samples"), fill = guide_legend(title = "Samples")) +
-    labs(x = "", y = "Mutaciones / software", title = "") +
+    labs(x = "", y = "Mutaciones / software", title = "Illumina software") +
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
     text = element_text(size = 15))
 
@@ -492,7 +492,7 @@ p2<- ggplot(subset(variants_data, plataforma == "Ion Torrent"), aes(x = software
         sec.axis = sec_axis(trans=~./25, name="Software")
     ) +
     guides(color = guide_legend(title = "Samples"), fill = guide_legend(title = "Samples")) +
-    labs(x = "", y = "", title = "") +
+    labs(x = "", y = "", title = "Ion Torrent software") +
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
     text = element_text(size = 15))
 
@@ -511,6 +511,421 @@ ggplot(bioinfo_data, aes(variant, fill = platform)) +
     theme(
     text = element_text(size = 15),
     axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
-
-p1 + p2
 ggsave("Graficos/qc_variantes_software.png", dpi = 500, units = "cm", width = 25, height = 25)
+
+
+### calculo de TP y FP - QC
+
+# linajes
+
+qc_parsed_linajes <- read_excel(dir_excel[1], sheet = 9
+df_parsed_linajes <- as.data.frame(qc_parsed_linajes[, c(3, 5:14)])
+
+# df_linajes_control <- df_parsed_linajes[df_parsed_linajes$grupo == "control", 2:11]
+df_linajes_control<- c("B.1.1.7",
+"B.1.351",
+"A.28",
+"B.1.621",
+"P.1",
+"AY.9.2",
+"AY.43",
+"AY.94",
+"AY.94",
+"AY.43")
+
+df_linajes_lab <- df_parsed_linajes[df_parsed_linajes$grupo != "control", 2:11]
+
+# calculamos los TP, FP, FN by sample
+
+matrix_tasa<- matrix(0, ncol = 10, nrow = 41)
+for (j in 1:ncol(df_linajes_lab)) {
+    control<- df_linajes_control[j]
+    muestra<- df_linajes_lab[, j]
+        for (i in 1:length(muestra)) {
+            if (control == muestra[i]) {
+                matrix_tasa [i, j]<- "TP"
+            } else if (muestra[i] == "None") {
+                matrix_tasa [i, j]<- "FN"
+            } else if (control != muestra[i]) {
+                matrix_tasa [i, j]<- "FP"
+            } 
+        }   
+}
+
+table(matrix_tasa[,1])
+table(matrix_tasa[,2])
+table(matrix_tasa[,3])
+table(matrix_tasa[,4])
+table(matrix_tasa[,5])
+table(matrix_tasa[,6])
+table(matrix_tasa[,7])
+table(matrix_tasa[,8])
+table(matrix_tasa[,9])
+table(matrix_tasa[,10])
+
+matrix_tasa<- matrix(0, ncol = 10, nrow = 41)
+for (j in 1:ncol(df_linajes_lab)) {
+    control<- df_linajes_control[j]
+    muestra<- df_linajes_lab[, j]
+        for (i in 1:length(muestra)) {
+            if (control == muestra[i]) {
+                matrix_tasa [i, j]<- "Bien"
+            } else if (muestra[i] == "None") {
+                matrix_tasa [i, j]<- "Mal"
+            } else if (control != muestra[i]) {
+                matrix_tasa [i, j]<- "Mal"
+            } 
+        }   
+}
+
+
+matrix_valores_1 <- matrix(0, ncol = 1, nrow = 41)
+for (i in 1:41) {
+    tabla_1 <- table(matrix_tasa[i, ])
+    matrix_valores_1[i, 1] <- as.numeric(tabla_1[1])
+}
+
+df_aciertos_lin<- data.frame(
+    id = df_parsed_linajes$grupo[-1],
+    aciertos = matrix_valores_1[,1]
+)
+
+
+# variantes
+
+qc_parsed_variantes <- read_excel(dir_excel[1], sheet = 10)
+df_parsed_variantes <- as.data.frame(qc_parsed_variantes[, c(1, 3:12)])
+
+# df_linajes_control <- df_parsed_linajes[df_parsed_linajes$grupo == "control", 2:11]
+df_variantes_control<- c("Alfa",
+"Beta",
+"None",
+"Mu",
+"Gamma",
+"Delta",
+"Delta",
+"Delta",
+"Delta",
+"Delta")
+
+df_variantes_lab <- df_parsed_variantes[df_parsed_variantes$grupo != "control", 2:11]
+
+# calculamos los TP, FP, FN by sample
+
+matrix_tasa<- matrix(0, ncol = 10, nrow = 41)
+for (j in 1:ncol(df_variantes_lab)) {
+    control<- df_variantes_control[j]
+    muestra<- df_variantes_lab[, j]
+        for (i in 1:length(muestra)) {
+            if (control == muestra[i]) {
+                matrix_tasa [i, j]<- "TP"
+            } else if (muestra[i] == "None") {
+                matrix_tasa [i, j]<- "FN"
+            } else if (control != muestra[i]) {
+                matrix_tasa [i, j]<- "FP"
+            } 
+        }   
+}
+
+table(matrix_tasa[,1])
+table(matrix_tasa[,2])
+table(matrix_tasa[,3])
+table(matrix_tasa[,4])
+table(matrix_tasa[,5])
+table(matrix_tasa[,6])
+table(matrix_tasa[,7])
+table(matrix_tasa[,8])
+table(matrix_tasa[,9])
+table(matrix_tasa[,10])
+
+matrix_tasa<- matrix(0, ncol = 10, nrow = 41)
+for (j in 1:ncol(df_variantes_lab)) {
+    control<- df_variantes_control[j]
+    muestra<- df_variantes_lab[, j]
+        for (i in 1:length(muestra)) {
+            if (control == muestra[i]) {
+                matrix_tasa [i, j]<- "Bien"
+            } else if (muestra[i] == "None") {
+                matrix_tasa [i, j]<- "Mal"
+            } else if (control != muestra[i]) {
+                matrix_tasa [i, j]<- "Mal"
+            } 
+        }   
+}
+
+
+matrix_valores_1 <- matrix(0, ncol = 1, nrow = 41)
+for (i in 1:41) {
+    tabla_1 <- table(matrix_tasa[i, ])
+    matrix_valores_1[i, 1] <- as.numeric(tabla_1[1])
+}
+
+df_aciertos_variantes<- data.frame(
+    id = df_parsed_linajes$grupo[-1],
+    aciertos = matrix_valores_1[,1]
+)
+
+
+# write.table(df_aciertos, "df_aciertos.csv", row.names = F, quote = F, sep = "\t")
+
+
+### calculo de TP y FP - Viralrecon
+
+qc_parsed_linajes <- read_excel(dir_excel[1], sheet = 9)
+df_parsed_linajes <- as.data.frame(qc_parsed_linajes)
+
+# linajes
+
+# df_linajes_control <- df_parsed_linajes[df_parsed_linajes$grupo == "control", 2:11]
+df_linajes_control<- c("B.1.1.7",
+"B.1.351",
+"A.28",
+"B.1.621",
+"P.1",
+"AY.9.2",
+"AY.43",
+"AY.94",
+"AY.94",
+"AY.43")
+
+df_linajes_lab <- df_parsed_linajes[df_parsed_linajes$grupo != "control", 2:11]
+
+# calculamos los TP, FP, FN by sample
+
+matrix_tasa<- matrix(0, ncol = 10, nrow = 24)
+for (j in 1:ncol(df_linajes_lab)) {
+    control<- df_linajes_control[j]
+    muestra<- df_linajes_lab[, j]
+        for (i in 1:length(muestra)) {
+            if (control == muestra[i]) {
+                matrix_tasa [i, j]<- "TP"
+            } else if (muestra[i] == "None") {
+                matrix_tasa [i, j]<- "FN"
+            } else if (control != muestra[i]) {
+                matrix_tasa [i, j]<- "FP"
+            } 
+        }   
+}
+
+table(matrix_tasa[,1])
+table(matrix_tasa[,2])
+table(matrix_tasa[,3])
+table(matrix_tasa[,4])
+table(matrix_tasa[,5])
+table(matrix_tasa[,6])
+table(matrix_tasa[,7])
+table(matrix_tasa[,8])
+table(matrix_tasa[,9])
+table(matrix_tasa[,10])
+
+matrix_tasa<- matrix(0, ncol = 10, nrow = 40)
+for (j in 1:ncol(df_linajes_lab)) {
+    control<- df_linajes_control[j]
+    muestra<- df_linajes_lab[, j]
+        for (i in 1:length(muestra)) {
+            if (control == muestra[i]) {
+                matrix_tasa [i, j]<- "Bien"
+            } else if (muestra[i] == "None") {
+                matrix_tasa [i, j]<- "Mal"
+            } else if (control != muestra[i]) {
+                matrix_tasa [i, j]<- "Mal"
+            } 
+        }   
+}
+
+
+matrix_valores_0 <- matrix(0, ncol = 1, nrow = 40)
+for (i in 1:40) {
+    tabla_0 <- table(matrix_tasa[i, ])
+    matrix_valores_0[i, 1] <- as.numeric(tabla_0[2])
+}
+
+matrix_valores_0[7,]<- 0
+matrix_valores_0[30,]<- 0
+matrix_valores_0[32,]<- 0
+matrix_valores_0[36,]<- 0
+matrix_valores_0[37,]<- 0
+
+df_aciertos<- data.frame(
+    id = df_parsed_linajes$grupo[-1],
+    aciertos = matrix_valores_0[,1]
+)
+
+
+
+#### Datos aciertos
+
+# sort_df_aciertos_lin<- df_aciertos_lin[order(df_aciertos_lin$id), ]
+# write.table(sort_df_aciertos_lin, "aciertos_linajes.csv", row.names = F, quote = F, sep = "\t")
+# sort_df_aciertos_variantes<- df_aciertos_variantes[order(df_aciertos_variantes$id), ]
+# write.table(sort_df_aciertos_variantes, "aciertos_variantes.csv", row.names = F, quote = F, sep = "\t")
+
+qc_aciertos <- read_excel(dir_excel[1], sheet = 15)
+niveles_aciertos<- c("HU Virgen del Rocio",
+"HU San Cecilio",
+"HU Miguel Servet",
+"CIB de Aragón",
+"HCU Lozano Blesa",
+"HUC de Asturias",
+"HU Son Espases",
+"HU de Gran Canaria Dr. Negrín",
+"HU Ntra. Sra de Candelaria",
+"HU Marqués de Valdecilla",
+"HU de Toledo",
+"CHU de Albacete",
+"HU de Ciudad Real",
+"Consorcio LUCIA",
+"HU Germans Trias i Pujol - 1",
+"HU Germans Trias i Pujol - 2",
+"H Dr. Josep Trueta",
+"LR de Catalunya",
+"HU Vall d’Hebron",
+"H Clínic de Barcelona",
+"Hospital Universitari Bellvitge - 1",
+"Hospital Universitari Bellvitge - 2",
+"Banc de Sang i Teixits Catalunya",
+"HU de Badajoz",
+"CHUVI",
+"CHUS",
+"CHUAC",
+"Fundacion RiojaSalud",
+"HU 12 DE OCTUBRE",
+"HU La Paz",
+"HU RAMÓN Y CAJAL",
+"HU GREGORIO MARAÑÓN",
+"HU Virgen de la Arrixaca",
+"CH de Navarra - 1",
+"CH de Navarra - 2",
+"HU de Donostia",
+"FISABIO - 1",
+"FISABIO - 2",
+"HGU de Alicante",
+"HGU de Valencia",
+"HGU de Elche")
+
+niveles_id_aciertos<-c("COD_2109",
+"COD_2129",
+"COD_2122",
+"COD_2126",
+"COD_2132",
+"COD_2143",
+"COD_2110",
+"COD_2102",
+"COD_2111",
+"COD_2104",
+"COD_2101",
+"COD_2127",
+"COD_2136",
+"COD_2131",
+"COD_2107",
+"COD_2107_2",
+"COD_2114",
+"COD_2115",
+"COD_2121",
+"COD_2123",
+"COD_2124",
+"COD_2124_2",
+"COD_2141",
+"COD_2119",
+"COD_2112",
+"COD_2135",
+"COD_2137",
+"COD_2113",
+"COD_2103",
+"COD_2105",
+"COD_2116",
+"COD_2125",
+"COD_2120",
+"COD_2117",
+"COD_2117_2",
+"COD_2108",
+"COD_2106",
+"COD_2106_2",
+"COD_2134",
+"COD_2139",
+"COD_2140")
+
+qc_aciertos$nombre<- factor(qc_aciertos$nombre, levels = niveles_aciertos)
+qc_aciertos$id<- factor(qc_aciertos$id, levels = niveles_id_aciertos)
+
+ggplot(qc_aciertos, aes(x = id, y = aciertos, fill = comunidad)) +
+    geom_bar(stat = "identity") +
+    coord_flip() +
+    facet_grid(~clase) +
+    guides(color = guide_legend(title = "Curves"), fill = guide_legend(title = "Comunidades")) +
+    labs(x = "Aciertos", y = "", title = "") +
+    theme(
+    text = element_text(size = 22),
+    axis.text.x = element_text(vjust = 1, hjust = 1))
+ggsave("Graficos/qc_aciertos.png")
+
+# Sensibilidad y precision
+
+levels_samples_lin<- c("Muestra 1 B.1.1.7",
+"Muestra 2 B.1.351",
+"Muestra 3 A.28",
+"Muestra 4 B.1.621",
+"Muestra 5 P.1",
+"Muestra 6 AY.9.2",
+"Muestra 7 AY.43",
+"Muestra 8 AY.94",
+"Muestra 9 AY.94",
+"Muestra 10 AY.43")
+
+levels_samples_var<- c("Muestra 1 Alfa",
+"Muestra 2 Beta",
+"Muestra 3 None",
+"Muestra 4 Mu",
+"Muestra 5 Gamma",
+"Muestra 6 Delta",
+"Muestra 7 Delta",
+"Muestra 8 Delta",
+"Muestra 9 Delta",
+"Muestra 10 Delta")
+
+qc_tasa <- read_excel(dir_excel[1], sheet = 14)
+
+qc_tasa_linajes<- subset(qc_tasa, clase == "linajes")
+qc_tasa_linajes$samples<- factor (qc_tasa_linajes$samples, levels = levels_samples_lin)
+qc_tasa_linajes$tipo<- factor (qc_tasa_linajes$tipo, levels = c("Sensitivity", "Precision"))
+
+qc_tasa_variantes<- subset(qc_tasa, clase == "variantes")
+qc_tasa_variantes$samples<- factor (qc_tasa_variantes$samples, levels = levels_samples_var)
+qc_tasa_variantes$tipo<- factor (qc_tasa_variantes$tipo, levels = c("Sensitivity", "Precision"))
+
+# mutaciones por muestra
+
+matrix_mutaciones <- matrix(0, ncol = 2, nrow = 10)
+for (i in 1:10) {
+    matrix_mutaciones[i, 1]<- mean(variants_data$variants[variants_data$muestra == unique(variants_data$muestra)[i]], na.rm = T)
+    matrix_mutaciones[i, 2]<- median(variants_data$variants[variants_data$muestra == unique(variants_data$muestra)[i]], na.rm = T)
+}
+
+qc_tasa_linajes$mutaciones<- round(as.numeric(rep (matrix_mutaciones[,1], 2)))
+qc_tasa_variantes$mutaciones<- round(as.numeric(rep (matrix_mutaciones[,1], 2)))
+
+qc_tasa_linajes$tipo<- revalue( qc_tasa_linajes$tipo, c("Sensitivity" = "Sensibilidad", "Precision" = "Precisión"))
+qc_tasa_variantes$tipo<- revalue( qc_tasa_variantes$tipo, c("Sensitivity" = "Sensibilidad", "Precision" = "Precisión"))
+
+# plot sensitivity, precision & mutations
+
+resultado_data<- rbind(qc_tasa_linajes, qc_tasa_variantes)
+prueba_nombres<- rep(nombres_muestras, 4)
+resultado_data$samples<- factor(prueba_nombres, levels = nombres_muestras)
+resultado_data$clase<- revalue(resultado_data$clase, c("linajes" = "Linajes", "variantes" = "Variantes"))
+
+ggplot(resultado_data, aes(x = samples, y = tasa * 100, group = tipo)) + 
+    geom_point(aes(x = samples, y = mutaciones), size = 3) +
+    geom_line(aes(color = tipo), size = 2) +
+    facet_grid(tipo~clase) +
+        scale_y_continuous(
+        name = "%",
+        sec.axis = sec_axis(trans=~./1, name="Mutaciones")
+    ) +
+    guides(color = guide_legend(title = "Curvas"), fill = guide_legend(title = "Platform")) +
+    labs(x = "", y = "%", title = "") +
+    theme(
+    text = element_text(size = 20),
+    axis.text.x = element_text(vjust = 1, hjust = 1, angle = 45))
+
+ggsave("Graficos/qc_sensitivity_precision_mutaciones.png")
