7c4753b240c2b03a88ac68396b78132814acb41c
Alema91 <alberto.lema.blanco@gmail.com>
2022-04-25 17:54:44 +0200

    Script consensus y versiones de pangolin: 2

--- qc_results/07_analysis_qc.R
+++ qc_results/07_analysis_qc.R
@@ -31,3 +31,49 @@ pangolin_data <- data.frame(
     linajes = as.character(qc_pangolin$lineage),
     version = as.character(qc_pangolin$version)
 )
+
+pangolin_data$muestra <- factor(pangolin_data$muestra, levels = nombres_muestras)
+
+# plots
+
+# v3.1.16
+
+ggplot(subset(pangolin_data, version == "v3.1.16"), aes(muestra, fill = linajes)) +
+    geom_bar() +
+    guides(fill = guide_legend(title = "")) +
+    labs(y = "v3.1.16", x = "", title = "") +
+    geom_text(stat = "count", position = position_stack(vjust = 0.5), aes(label = after_stat(count)), size = 12) +
+    theme(
+        text = element_text(size = 55),
+        axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)
+    )
+ggsave("Graficos/qc_pangolin_16.png", width = 75, height = 65, dpi = 300, units = c("cm"))
+
+
+# todas
+
+ggplot(pangolin_data, aes(muestra, fill = linajes)) +
+    geom_bar() +
+    guides(fill = guide_legend(title = "")) +
+    facet_grid(~version) +
+    labs(y = "", x = "", title = "") +
+    geom_text(stat = "count", position = position_stack(vjust = 0.5), aes(label = after_stat(count)), size = 12) +
+    theme(
+        text = element_text(size = 55),
+        axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)
+    )
+ggsave("Graficos/qc_pangolin.png", width = 75, height = 65, dpi = 300, units = c("cm"))
+
+# todas
+
+ggplot(subset(pangolin_data, ), aes(muestra, fill = linajes)) +
+    geom_bar() +
+    guides(fill = guide_legend(title = "")) +
+    facet_grid(~version) +
+    labs(y = "", x = "", title = "") +
+    geom_text(stat = "count", position = position_stack(vjust = 0.5), aes(label = after_stat(count)), size = 12) +
+    theme(
+        text = element_text(size = 55),
+        axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)
+    )
+ggsave("Graficos/qc_pangolin.png", width = 75, height = 65, dpi = 300, units = c("cm"))
