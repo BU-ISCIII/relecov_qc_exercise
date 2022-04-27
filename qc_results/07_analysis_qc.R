d568faff10e1f4d099bbc2d843796f364067fca9
Alema91 <alberto.lema.blanco@gmail.com>
2022-04-27 17:51:13 +0200

    wide table: 1

--- qc_results/07_analysis_qc.R
+++ qc_results/07_analysis_qc.R
@@ -33,36 +33,22 @@ pangolin_data_lab <- data.frame(
 )
 
 pangolin_data_lab$muestra <- factor(pangolin_data_lab$muestra, levels = nombres_muestras)
-pangolin_data_lab$programa <- as.character(rep("Laboratorios", length(pangolin_data_lab$grupo)))
+# pangolin_data_lab$programa <- as.character(rep("Laboratorios", length(pangolin_data_lab$grupo)))
+
 
 # plots
 
-# v3.1.16
-
-ggplot(subset(pangolin_data_lab, version == "v3.1.16"), aes(muestra, fill = linajes)) +
-    geom_bar() +
-    guides(fill = guide_legend(title = "")) +
-    labs(y = "v3.1.16", x = "", title = "") +
-    geom_text(stat = "count", position = position_stack(vjust = 0.5), aes(label = after_stat(count)), size = 12) +
-    theme(
-        text = element_text(size = 55),
-        axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)
-    )
-# ggsave("Graficos/qc_pangolin_16.png", width = 75, height = 65, dpi = 300, units = c("cm"))
-
-# TODAS
-
 ggplot(pangolin_data_lab, aes(muestra, fill = linajes)) +
     geom_bar() +
-    guides(fill = guide_legend(title = "")) +
     facet_grid(~version) +
+    guides(fill = guide_legend(title = "")) +
     labs(y = "", x = "", title = "") +
-    geom_text(stat = "count", position = position_stack(vjust = 0.5), aes(label = after_stat(count)), size = 12) +
+    geom_text(stat = "count", position = position_stack(vjust = 0.5), aes(label = after_stat(count)), size = 10) +
     theme(
-        text = element_text(size = 55),
+        text = element_text(size = 45),
         axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)
     )
-ggsave("Graficos/qc_pangolin.png", width = 75, height = 65, dpi = 300, units = c("cm"))
+ggsave("Graficos/qc_pangolin_lab.png", width = 75, height = 55, dpi = 300, units = c("cm"))
 
 ### datos viralrecon ----
 
@@ -82,23 +68,9 @@ pangolin_data_viralrecon$programa <- as.character(rep("Viralrecon", length(pango
 
 # plots
 
-# v3.1.16
-
-ggplot(subset(pangolin_data_viralrecon, version == "v3.1.16"), aes(muestra, fill = linajes)) +
-    geom_bar() +
-    guides(fill = guide_legend(title = "")) +
-    labs(y = "v3.1.16", x = "", title = "") +
-    geom_text(stat = "count", position = position_stack(vjust = 0.5), aes(label = after_stat(count)), size = 12) +
-    theme(
-        text = element_text(size = 55),
-        axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)
-    )
-ggsave("Graficos/qc_pangolin_16.png", width = 75, height = 65, dpi = 300, units = c("cm"))
-
-# TODAS
-
 ggplot(pangolin_data_viralrecon, aes(muestra, fill = linajes)) +
     geom_bar() +
+    facet_grid(~version) +
     guides(fill = guide_legend(title = "")) +
     facet_grid(~version) +
     labs(y = "", x = "", title = "") +
@@ -109,39 +81,81 @@ ggplot(pangolin_data_viralrecon, aes(muestra, fill = linajes)) +
     )
 ggsave("Graficos/qc_pangolin_viralrecon.png", width = 75, height = 65, dpi = 300, units = c("cm"))
 
+### datos control ----
+
+qc_pangolin <- read_excel(dir_excel[1], sheet = 3)
+
+nombres_muestras <- c("muestra 1", "muestra 2", "muestra 3", "muestra 4", "muestra 5", "muestra 6", "muestra 7", "muestra 8", "muestra 9", "muestra 10")
+
+pangolin_data_control <- data.frame(
+    grupo = as.character(qc_pangolin$group),
+    muestra = as.character(qc_pangolin$sample),
+    linajes = as.character(qc_pangolin$lineage),
+    version = as.character(qc_pangolin$version)
+)
+
+pangolin_data_control$muestra <- factor(pangolin_data_control$muestra, levels = nombres_muestras)
+pangolin_data_control$programa <- as.character(rep("Control", length(pangolin_data_control$grupo)))
+
+# plots
+
+ggplot(pangolin_data_control, aes(muestra, fill = linajes)) +
+    geom_bar() +
+    facet_grid(~version) +
+    guides(fill = guide_legend(title = "")) +
+    facet_grid(~version) +
+    labs(y = "", x = "", title = "") +
+    geom_text(stat = "count", position = position_stack(vjust = 0.5), aes(label = after_stat(count)), size = 12) +
+    theme(
+        text = element_text(size = 55),
+        axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)
+    )
+ggsave("Graficos/qc_pangolin_control.png", width = 75, height = 65, dpi = 300, units = c("cm"))
+
+
 # Analisis en conjunto
 
-data_conjunto<- rbind(pangolin_data_lab, pangolin_data_viralrecon)
+data_conjunto <- rbind(pangolin_data_lab, pangolin_data_viralrecon)
+str(data_conjunto)
 
-grupos_eliminar<- c(
-"COD_2101",
-"COD_2102",
-"COD_2104",
-"COD_2105",
-"COD_2106_Nanopore",
-"COD_2107_Nanopore",
-"COD_2115",
-"COD_2119",
-"COD_2120",
-"COD_2127",
-"COD_2136",
-"COD_2140",
-"COD_2143"
+# wide table
+
+wide_pangolin <- data_conjunto %>% pivot_wider(names_from = version, values_from = linajes)
+
+head(wide_pangolin)
+
+# write.table(wide_pangolin, "df_conjunto_wide.csv", row.names = F, sep = "\t", quote = F)
+
+
+grupos_eliminar <- c(
+    "COD_2101",
+    "COD_2102",
+    "COD_2104",
+    "COD_2105",
+    "COD_2106_Nanopore",
+    "COD_2107_Nanopore",
+    "COD_2115",
+    "COD_2119",
+    "COD_2120",
+    "COD_2127",
+    "COD_2136",
+    "COD_2140",
+    "COD_2143"
 )
 
 `%notin%` <- Negate(`%in%`)
-df_conjunto<- data_conjunto[data_conjunto$grupo %notin% grupos_eliminar, ]
+df_conjunto <- data_conjunto[data_conjunto$grupo %notin% grupos_eliminar, ]
 
 # write.table(df_conjunto, "df_conjunto.csv", row.names = F, sep = "\t", quote = F)
 
 # TODAS
 
-n_df_conjunto<- na.exclude(df_conjunto)
+n_df_conjunto <- na.exclude(df_conjunto)
 
 ggplot(n_df_conjunto, aes(muestra, fill = linajes)) +
     geom_bar() +
     guides(fill = guide_legend(title = "")) +
-    facet_grid(programa~version) +
+    facet_grid(programa ~ version) +
     labs(y = "", x = "", title = "") +
     geom_text(stat = "count", position = position_stack(vjust = 0.5), aes(label = after_stat(count)), size = 12) +
     theme(
